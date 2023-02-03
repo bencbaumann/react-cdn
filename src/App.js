4e0c65d8357b768c429da6680caf274b5f342e5a
Benjamin Baumann <ben@benjaminslaptop.lan>
2023-02-02 20:31:22 -0500

    testing access to liquid variable

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <h1>Hello From Ben & Michelle</h1>
         <p>Hopefully we are making app progress</p>
+        <p>Let's see if we can accss this products sku: {window.sku}</p>
       </header>
     </div>
   );
